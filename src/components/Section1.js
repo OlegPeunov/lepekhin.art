fbee6f335f838f208ae5b9ee32af8f24a482c48d
Oleg Peunov <olegpeunov@yandex.ru>
2022-02-08 10:33:07 +0300

    two main sections  added

---
+++ src/components/Section1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Section1() {
+  return (
+    <div className="section section_color_blue">
+      <h1>Контент 1</h1>
+    </div>
+  );
+}
+
+export default Section1;
---
+++ src/components/Section2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Section2() {
+  return (
+    <div className="section section_color_yellow">
+      <h1>Контент 2</h1>
+    </div>
+  );
+}
+
+export default Section2;
